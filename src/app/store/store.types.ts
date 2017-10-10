0a5a3205c20ab572a59fe7da28efd3caf5f8f2d1
aminpaks <amin.pakseresht@hotmail.com>
2017-10-10 19:04:24 -0400

    Adds License

--- src/app/store/store.types.ts
+++ src/app/store/store.types.ts
@@ -6,26 +6,3 @@ import { FeatureXState } from 'app/feature-x/store';
 export interface AppState {
   featureX: FeatureXState;
 }
-
-export interface ActionCreator<Payload> {
-  new (payload?: Payload, error?: boolean): Action<Payload>;
-  type: string;
-  parse: (action: PlainAction<Payload>) => Action<Payload>;
-}
-
-export interface Action<Payload = any> extends PlainAction<Payload> {
-  type: string;
-  error: boolean;
-  payload: Payload;
-  toPlain: () => PlainAction<Payload>;
-  asObservable: () => Observable<PlainAction<Payload>>;
-}
-
-export interface PlainAction<Payload = any> extends SimpleAction {
-  type: string;
-  error: boolean;
-  payload: Payload;
-}
-
-export type Dispatch<A> = (action: A) => A;
-export type Middleware<S, A> = (store: NgRedux<S>) => (dispatch: Dispatch<A>) => Dispatch<A>;
--- src/app/store/store.utils.ts
+++ src/app/store/store.utils.ts
@@ -1,8 +1,37 @@
+/**
+ * @license ActionCreator
+ * (c) 2017 Amin Paks <amin.pakseresht@hotmail.com>
+ * License: MIT
+ */
+
 import 'rxjs/add/observable/of';
 import { Action as SimpleAction } from 'redux';
 import { NgRedux } from '@angular-redux/store';
 import { Observable } from 'rxjs/Observable';
-import { AppState, Middleware, ActionCreator, Action, PlainAction } from './store.types';
+import { AppState } from './store.types';
+
+export type Dispatch<A> = (action: A) => A;
+export type Middleware<S, A> = (store: NgRedux<S>) => (dispatch: Dispatch<A>) => Dispatch<A>;
+
+export interface ActionCreator<Payload> {
+  new (payload?: Payload, error?: boolean): Action<Payload>;
+  type: string;
+  parse: (action: PlainAction<Payload>) => Action<Payload>;
+}
+
+export interface Action<Payload = any> extends PlainAction<Payload> {
+  type: string;
+  error: boolean;
+  payload: Payload;
+  toPlain: () => PlainAction<Payload>;
+  asObservable: () => Observable<PlainAction<Payload>>;
+}
+
+export interface PlainAction<Payload = any> extends SimpleAction {
+  type: string;
+  error: boolean;
+  payload: Payload;
+}
 
 /**
  * A simple solution to turn an enriched action to a simple plain action
@@ -19,14 +48,14 @@ export const convertActionMiddleware: Middleware<AppState, SimpleAction> = store
 
 export function actionCreator<Payload = undefined>(actionType: string): ActionCreator<Payload> {
   const safeClassName = getSafeClassName(actionType);
-  const actionGenerator = Function(safeClassName, `
+  const actionGenerator = new Function(`
     return function ${safeClassName}(payload, error) {
       this.type = "${actionType}";
       this.payload = payload == void 0 ? undefined : payload;
       this.error = error == void 0 ? false : true;
     };
   `); // `;
-  const action = actionGenerator();
+  const action: any = actionGenerator();
   action.type = actionType;
   action.parse = function (simple: PlainAction<Payload>) {
     return new action(simple.payload, simple.error);
